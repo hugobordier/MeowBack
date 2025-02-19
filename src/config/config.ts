729c3b2349c1f5ef0d2e05b267aa663d0242142b
hugobordier <hugo.bordier2003@gmail.com>
2025-02-19 11:51:24 +0100

    db en plus clair

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,18 +5,15 @@ dotenv.config();
 
 const databaseUrl = process.env.DATABASE_URL as string;
 
-const db = new Sequelize(
-  'postgresql://neondb_owner:npg_5OCjEl1GSIub@ep-young-forest-a9picu0i-pooler.gwc.azure.neon.tech/neondb?sslmode=require',
-  {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
+const db = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
     },
-  }
-);
+  },
+});
 
 // const db = new Sequelize('meowdb', 'hugo', 'admin', {
 //   host: 'localhost',
