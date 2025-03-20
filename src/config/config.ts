9d1d9096b16810996158f2b74b14e1270b2f4c2f
hugobordier <hugo.bordier2003@gmail.com>
2025-03-20 12:30:20 +0100

    change config db to host online

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,21 +6,21 @@ dotenv.config();
 const databaseUrl = process.env.DATABASE_URL as string;
 
 // //la db en ligne
-// const db = new Sequelize(databaseUrl, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//     },
-//   },
-// });
+const db = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
 
 // travailler avec la db locale
-const db = new Sequelize('meowdb', 'hugo', 'admin', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+// const db = new Sequelize('meowdb', 'hugo', 'admin', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
 
 //test
 
