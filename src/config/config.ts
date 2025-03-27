71d40f1269ff5d15aef58b695d59c89d1ef1a612
hugobordier <hugo.bordier2003@gmail.com>
2025-03-27 12:40:35 +0100

    encore mis la bonne db

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,20 +5,20 @@ dotenv.config();
 
 const databaseUrl = process.env.DATABASE_URL as string;
 
-// const db = new Sequelize(databaseUrl, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//     },
-//   },
-// });
-
-const db = new Sequelize('meowdb', 'hugo', 'admin', {
-  host: 'localhost',
+const db = new Sequelize(databaseUrl, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
+
+// const db = new Sequelize('meowdb', 'hugo', 'admin', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
 
 //test
 
