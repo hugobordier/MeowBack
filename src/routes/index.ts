e0a58048f324f1454f146a82fdafe89faa647dc8
Hippolyte <hippolyte.texier--bourrouilh@epfedu.fr>
2025-03-29 14:23:10 +0100

    toutes les routes mènent à l'index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
 import SwaggerRouter from '../swagger-builder/SwaggerRouter';
 import authRoutes from './authRoutes';
 import PetsitterRoutes from './PetsitterRoute';
-<<<<<<< HEAD
 import UserRoutes from './UserRoute';
-=======
 import PetRoutes from './PetRoutes';
->>>>>>> ad3dd95 (CRUD pet marche db locale mais pas encore db en ligne)
 
 export interface IRoute {
   path: string;
@@ -23,16 +20,13 @@ const routes: IRoute[] = [
     path: '/Petsitter',
     route: PetsitterRoutes,
   },
-<<<<<<< HEAD
   {
     path: '/User',
     route: UserRoutes,
   },
-=======
   {path:'/PetsRoutes',
     route: PetRoutes,
   }
->>>>>>> ad3dd95 (CRUD pet marche db locale mais pas encore db en ligne)
 ];
 
 routes.forEach((r) => {
