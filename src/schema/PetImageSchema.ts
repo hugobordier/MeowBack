c5540a0405e5461e80ca8a387413cb0883b4ead0
Hippolyte <hippolyte.texier--bourrouilh@epfedu.fr>
2025-05-02 10:12:06 +0200

    ca reprend petimages et pet

--- src/schema/PetImageSchema.ts
+++ src/schema/PetImageSchema.ts
@@ -9,6 +9,7 @@ export const petImageSchema = z.object({
 
 export const petImagePatchSchema = z.object({
   id: z.string().uuid().min(1, "L'id de l'image remplacée est requis"),
+  //pet_id : z.string().uuid().min(1, "L'url du pet est requise"),
   url_image: z.string().min(1, "L'url de l'image est requise"),
   createdAt: z.string().optional(),
   updatedAt: z.string().optional(),
