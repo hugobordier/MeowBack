bae9008ebbaad7e0ac2f54e6fcd4635e30db5d0c
aleks2705 <aleksei.najmitdinov@gmail.com>
2025-06-13 12:59:15 +0200

    feat[messenger] ..

--- src/models/Messenger.ts
+++ src/models/Messenger.ts
@@ -42,7 +42,7 @@ Messenger.init(
             defaultValue: false,
         },
         roomId:{
-            type: DataTypes.UUID,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
     },
